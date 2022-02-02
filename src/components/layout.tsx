01daceb1be0108e8fae6d998eaf4d77df1054b84
Hugh Lee <u88803494@gmail.com>
2022-02-02 12:46:46 +0800

    Adjust the style of the website

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -16,7 +16,7 @@ const Layout = ({ children }: ILayout) => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Navbar />
-      <div className="min-h-screen w-screen px-6 pt-16">
+      <div className="min-h-screen w-screen bg-slate-800 px-6 pt-20 text-gray-100">
         <main className="w-screen-xl mx-auto max-w-screen-xl">{children}</main>
       </div>
     </>
--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -1,9 +1,10 @@
 import { ChangeEvent, useEffect, useState } from 'react';
 import Link from 'next/link';
 
-import { Menu, QuestionMark } from '../svg';
 import { useAppDispatch } from 'features/hook';
 import { getDefinitions } from 'features/dictionary/dictionary.service';
+import { Menu, QuestionMark } from '../svg';
+import { navbarItems } from './config';
 
 const Navbar = () => {
   const dispatch = useAppDispatch();
@@ -38,22 +39,21 @@ const Navbar = () => {
   const navigationMdStyle = 'md:static md:flex md:items-center md:justify-between md:shadow-none';
 
   return (
-    <nav className="fixed h-16 w-screen bg-white px-6 shadow-md">
+    <nav className="fixed h-16 w-screen border-b border-slate-700 bg-slate-900 px-6 text-gray-100">
       <div className="w-screen-xl relative mx-auto flex h-full max-w-screen-xl justify-between">
         <div className="flex items-center">
-          <div className="whitespace-nowrap text-3xl font-bold text-gray-800 hover:text-gray-800">新典</div>
-          <div className={`absolute top-16 -right-6 bg-white shadow-md ${navigationMdStyle} ${navigationDisplay}`}>
-            <div className="flex flex-col px-5 md:flex-row">
-              <Link href="/">
-                <a className="rounded py-2 px-2 text-gray-800 hover:bg-gray-900 hover:font-medium hover:text-gray-100 md:mx-2">
-                  Home
-                </a>
-              </Link>
-              <Link href="/about">
-                <a className="rounded py-2 px-2 text-gray-800 hover:bg-gray-900 hover:font-medium hover:text-gray-100 md:mx-2">
-                  About
-                </a>
-              </Link>
+          <div className="hover whitespace-nowrap text-3xl font-bold">新典</div>
+          <div
+            className={`absolute top-[60px] -right-6 bg-slate-900 shadow-md shadow-slate-900 ${navigationMdStyle} ${navigationDisplay}`}
+          >
+            <div className="flex flex-col px-5 py-2 md:flex-row">
+              {navbarItems.map(({ path, pathName }) => (
+                <Link key={path} href={path}>
+                  <a className="rounded py-2 px-2 hover:bg-slate-400 hover:font-medium hover:text-black md:mx-2">
+                    {pathName}
+                  </a>
+                </Link>
+              ))}
             </div>
           </div>
         </div>
@@ -61,20 +61,20 @@ const Navbar = () => {
           <div className="item-center flex">
             <div className="flex items-center">
               <input
-                className="focus:shadow-outline mx-4 h-10 rounded bg-gray-900 px-2 text-lg leading-tight text-gray-400 placeholder-gray-200 focus:outline-none"
+                className="focus:shadow-outline mx-4 h-10 rounded bg-slate-600 px-2 text-lg leading-tight placeholder-gray-200 focus:outline-none"
                 onChange={handleChangeWord}
                 placeholder="search"
                 type="text"
                 value={word}
               />
             </div>
-            <button className="hidden text-gray-900 hover:text-gray-700 md:block">
+            <button className="hidden text-white hover:text-gray-100 md:block">
               <Menu />
             </button>
           </div>
           <div className="flex items-center md:hidden">
             <button
-              className="block text-gray-900 hover:text-gray-700 focus:text-gray-700 focus:outline-none"
+              className="block text-white hover:text-gray-100 focus:text-gray-100 focus:outline-none"
               onClick={handleShowNavigation}
               type="button"
             >
