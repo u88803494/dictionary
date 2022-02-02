779ac53a7cbc5ecc03471445b458c5c4c8ce67e4
Hugh Lee <u88803494@gmail.com>
2022-02-02 21:48:24 +0800

    Correct the layout of the definitions

--- src/components/Dictionary/WordClass.tsx
+++ src/components/Dictionary/WordClass.tsx
@@ -2,9 +2,11 @@ import { IDefinition, IWordClassesDefinition } from 'redux/dictionary';
 
 const WordClass = ({ definitions, wordClass }: IWordClassesDefinition): JSX.Element => (
   <div className="mb-8">
-    <div className="border-b  border-slate-500 text-2xl">
-      <div className="flex w-8 justify-center bg-slate-300 text-slate-700">{wordClass}</div>
-    </div>
+    {wordClass && (
+      <div className="border-b  border-slate-500 text-2xl">
+        <div className="flex w-8 justify-center bg-slate-300 text-slate-700">{wordClass}</div>
+      </div>
+    )}
     {definitions.map(({ def, example, quote, synonyms }: IDefinition, index: number) => (
       <div className="my-3" key={def}>
         <div className="text-lg font-medium">{`${index + 1}. ${def}`}</div>
