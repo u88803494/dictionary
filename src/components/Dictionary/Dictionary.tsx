b0b813c1be4ea44eda3727d767d8bc63e14a8cae
Hugh Lee <u88803494@gmail.com>
2022-02-02 21:17:55 +0800

    Complete displaying the basic dictionary

--- src/components/Dictionary/Dictionary.tsx
+++ src/components/Dictionary/Dictionary.tsx
@@ -5,31 +5,27 @@ import WordClass from './WordClass';
 
 const Dictionary = (): JSX.Element => {
   const heteronyms = useAppSelector((state) => state.dictionary.heteronyms);
-
   return (
     <div className="space-y-12">
-      {heteronyms.map(({ key, pronunciations, wordClassesDefinitions }: IHeteronym) => {
-        return (
-          <div className="mt-2" key={key}>
-            <div className="flex flex-wrap">
-              {pronunciations &&
-                pronunciations.map(({ key, pronunciation1, pronunciation2, word }) => (
-                  <Pronunciation key={key} {...{ pronunciation1, pronunciation2, word }} />
-                ))}
-            </div>
-            <ul className="flex tracking-[0.5rem] text-slate-300">
-              <li className="mr-2 rounded-t-lg border-b border-slate-600 bg-slate-700 py-3 px-4 text-lg font-medium">
-                釋義
-              </li>
-            </ul>
-            <div className="rounded-b-xl rounded-tr-xl bg-slate-700 px-4 pt-2 pb-8">
-              {wordClassesDefinitions.map(({ definitions, wordClass }: IWordClassesDefinition) => (
-                <WordClass key={wordClass} {...{ definitions, wordClass }} />
-              ))}
-            </div>
+      {heteronyms.map(({ key, pronunciations, wordClassesDefinitions }: IHeteronym) => (
+        <div className="mt-2" key={key}>
+          <div className="flex flex-wrap">
+            {pronunciations.map(({ key, pronunciation1, pronunciation2, word }) => (
+              <Pronunciation key={key} {...{ pronunciation1, pronunciation2, word }} />
+            ))}
           </div>
-        );
-      })}
+          <ul className="flex tracking-[0.5rem] text-slate-300">
+            <li className="mr-2 rounded-t-lg border-b border-slate-600 bg-slate-700 py-3 px-4 text-lg font-medium">
+              釋義
+            </li>
+          </ul>
+          <div className="rounded-b-xl rounded-tr-xl bg-slate-700 px-4 py-2">
+            {wordClassesDefinitions.map(({ definitions, wordClass }: IWordClassesDefinition) => (
+              <WordClass key={wordClass} {...{ definitions, wordClass }} />
+            ))}
+          </div>
+        </div>
+      ))}
     </div>
   );
 };
