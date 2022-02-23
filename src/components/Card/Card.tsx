0df76fa3311ef46dc946f63ce53a140259e24dfb
Hugh Lee <u88803494@gmail.com>
2022-02-23 23:09:59 +0800

    Complete the layout of the Card

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -22,13 +22,13 @@ const Card = (props: IAttributes): JSX.Element => {
   const currencySymbol = currency === 'USD' ? '$' : 'NT$';
 
   return (
-    <div className="min-w-72 w-72 snap-center rounded-lg bg-white">
+    <div className="min-w-72 w-72 snap-center rounded-lg bg-white shadow-md">
       <div className="h-fit w-72 overflow-hidden rounded-t-lg">
         <Image src={shopCoverImageUrl} alt="shopCoverImageUrl" width="288" height="140" />
       </div>
       <div className="flex h-80 flex-col justify-between px-5 pt-4 pb-5">
         <div className="space-y-2">
-          <div className="text-xs text-gray-600">{studentsCount}人一起學習</div>
+          <div className="text-xs text-gray-500">{studentsCount}人一起學習</div>
           <div className="text-lg">{name}</div>
           <div className="overflow-hidden text-ellipsis whitespace-nowrap text-gray-400">{slogan}</div>
           <div className="text-xs">
@@ -43,10 +43,10 @@ const Card = (props: IAttributes): JSX.Element => {
             ))}
           </div>
         </div>
-        <div className="">
-          <div className="price">
-            <span>{currencySymbol + salePrice}</span>
-            <span>{currencySymbol + listPrice}</span>
+        <div>
+          <div className="space-x-2 text-sm leading-6">
+            <span className="text-red-500">{currencySymbol + salePrice}</span>
+            <span className="text-xs text-gray-400 line-through">定價 {currencySymbol + listPrice}</span>
           </div>
           <div className="flex justify-between">
             <Button color="red" productableId={productableId} productableType={productableType}>
